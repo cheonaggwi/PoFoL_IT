9bcfe1e19191b18a1f55a1e3040bd52b73e4fdc2
cheonaggwi <cheonaggwi@gmail.com>
2024-06-08 02:06:12 +0900

    매직 파일 5

--- Magic_Combination/Magic_combination.js
+++ Magic_Combination/Magic_combination.js
@@ -1,33 +1,54 @@
-var magic_id_object = {
-    "fire":0,
-    "water":0
+var magic_list = [[],[]]
+
+const magic_recipe = {
+    "fire":[["fog"],["flame","recovery"],["unquenchable fire"]],
+    "water":[["fog","recovery"],["flood"],["Tsunami"]],
+    "Null":["flame","flood","fog"]
 }
 
-var magic_object = {
-    "fire":0,
-    "water":0
+const mob_object = {
+
 }
 
-var object_count = 0
+var click_count = 0 ,magic_count = 0, mob_count = 0
 
 var add_input = function(text){
-    $("#select_container").append('<input type="button" id="'+"'"+text+"'"+'" onclick="selection_input('+"'"+text+"'"+')" hidden><label for="'+"'"+text+"'"+'">'+text+'</label>')
+    if(magic_count>1){
+        var add_magic = Object.values(magic_recipe[magic_list[0][0]][magic_list[1][0]-1]).filter(item => Object.values(magic_recipe[magic_list[0][1]][magic_list[1][1]-1]).includes(item));
+        if (add_magic.length == 1){
+            $("#select_container").append('<input type="button" id="'+"'"+add_magic[0]+"'"+'" onclick="selection_input('+"'"+add_magic[0]+"'"+')" hidden><label for="'+"'"+add_magic[0]+"'"+'">'+add_magic[0]+'</label>')
+        }
+    }
 }
 
 var selection_input = function(text){
-    if(object_count < 3){
-        object_count++
-        magic_id_object[text]++
-        magic_object[text]++
-        texts = text+magic_id_object[text]
-        $("#game_container").append('<input type="button" id="'+texts+'" onclick="remove_input('+"'"+texts+"'"+','+"'"+text+"'"+')" hidden><label for="'+texts+'">'+text+'</label>')
+    if(magic_count < 3){
+        i = magic_list[0].indexOf(text)
+        if(i != -1){
+            magic_list[1][i]++;
+        }
+        else{
+            magic_list[0].push(text);
+            magic_list[1].push(1);
+        }
+        i = magic_list[0].indexOf(text)
+
+        click_count++
+        magic_count++
+
+        $("#game_container").append('<input type="button" id="'+click_count+'" onclick="remove_input('+"'"+click_count+"'"+','+"'"+text+"'"+')" hidden><label for="'+click_count+'">'+text+'</label>');
     }
 }
 
 var remove_input = function(texts,text){
+    i = magic_list[0].indexOf(text)
+    magic_list[1][i]--;
+    if(magic_list[1][i] == 0){
+        magic_list[0].splice(i)
+        magic_list[1].splice(i)
+    }
+
     $("#"+texts+"+label").remove();
     $("#"+texts).remove();
-    magic_object[text]--
-    object_count--
-    console.log(magic_object[text])
+    magic_count--
 }
