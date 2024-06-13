0e824456b27ef6eb98ca4f70c6035e274f59b9c0
cheonaggwi <cheonaggwi@gmail.com>
2024-06-14 02:32:30 +0900

    매직 파일 12

--- Magic_Combination/Magic_combination.js
+++ Magic_Combination/Magic_combination.js
@@ -1,23 +1,31 @@
 var magic_list = [[],[]]
 
-var selection_list = []
+var selection_list = [[],[]]
 
 var mouse_position = [0,0]
 
 var imformation_object = {}
 
 const magic_recipe = {
-    "fire":[["fog","void","dust"],["flame","recovery"],["unquenchable fire"]],
-    "water":[["fog","recovery","tree","void"],["flood"],["tsunami"]],
-    "grass":[["tree","void","dust"],[],["earthquake"]],
-    "Null":["flame","flood","fog","tree","dust"]
+    "fire":[["2fog","3void","2dust"],["2flame","3recovery"],["3unquenchable fire"]],
+    "water":[["2fog","3recovery","2tree","3void","4sealed world tree"],["2flood"],["3tsunami"]],
+    "grass":[["2tree","3void","2dust"],[],["3earthquake"]],
+    "void":[[],[],["5BLACK HOLE"]],
+    "flame":[["5VOLCANO"],[],[]],
+    "dust":[["5VOLCANO"],[],[]],
+    "earthquake":[["5VOLCANO"],[],[]],
+    "unquenchable fire":[["4eternal life"],[],[]],
+    "recovery":[["4eternal life"],[],[]],
+    "tree":[["4sealed world tree"],[],[]],
+    "eternal life":[["4sealed world tree"],[],[]],
+    "Null":["2flame","2flood","2fog","2tree","2dust","4eternal life"]
 }
 
 var click_count = 0 ,magic_count = 0
 
-var select_container_z_index = function(z){
-    $('.select_container').css('z-index','0');
-    $('#select_container'+z).css('z-index','1');
+var select_container_display = function(z){
+    $('.select_container').css('display','none');
+    $('#select_container'+z).css('display','grid');
 }
 
 var add_input = function(){
@@ -33,17 +41,19 @@ var add_input = function(){
             add_magic = add_magic.filter(item => Object.values(magic_recipe["Null"]).includes(item));
         }
         if (add_magic.length == 1){
-            i = selection_list.indexOf(add_magic[0])
+            add_magic = [(""+add_magic[0]).substring(1),(""+add_magic[0]).substring(0,1)]
+            i = selection_list[0].indexOf(add_magic[0])
             if(i == -1){
-                new_magic_add(add_magic[0],magic_count)
+                new_magic_add(add_magic[0],add_magic[1])
             }
         }
     }
 }
 
 var new_magic_add = function(magic,star){
-    selection_list.push(magic)
-    imformation_object[magic] = [""+star,[""+magic_list[0]],[""+magic_list[1]]]
+    selection_list[0].push(magic)
+    selection_list[1].push(Number(star))
+    imformation_object[magic] = [""+star,(""+magic_list[0]).split(","),(""+magic_list[1]).split(",")]
     $("#select_container0 , #select_container"+star).append('<input type="button" id="'+"'"+magic+"'"+'" onclick="selection_input('+"'"+magic+"'"+')" class="magic" hidden><label for="'+"'"+magic+"'"+'" ><img src="magic_image/'+magic+'.png" width="100%" height="90%" >'+magic+'</label>')
 }
 
@@ -89,7 +99,15 @@ $(document).on('mouseenter','.magic+label', function() {
     if ($('#imformation_checkbox').is(':checked')) {
         $(this).css('background-color', 'yellow');
         $("#imformation").css('display', 'none');
-        $("#imformation").text(imformation_object[$(this).text()][0])
+        $("#imformation #imformation_detail").text(imformation_object[$(this).text()][0])
+        var text = ''
+        for(var i=0;i<imformation_object[$(this).text()][1].length; i++){
+            for(var j = 0; j<imformation_object[$(this).text()][2][i]; j++){
+                text = text+'<img src="magic_image/'+imformation_object[$(this).text()][1][i]+'.png" width="100%" height="90%" >'
+            }
+        }
+        $("#imformation #making_recipe").html(text)
+
     }
 
 }).on('mouseleave','.magic+label', function() {
